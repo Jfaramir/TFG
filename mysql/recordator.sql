1170da84c6ccf7bb6c85798606c9322f76bde9ed
pablo torres searle <pablots89@gmail.com>
2019-04-30 00:03:31 +0200

    base de datos modificada

--- mysql/recordator.sql
+++ mysql/recordator.sql
@@ -6,24 +6,27 @@ USE recordator;
 
 
 CREATE TABLE `usuarios` (
-
+--hay que poner un id 
 email varchar (50),
 nombre varchar (50),
  PRIMARY KEY (`email`)
 ) ;
 
 CREATE TABLE `grupos` (
-id int(10) auto_increment,
+id int(100) auto_increment,
 nombre Varchar(50),
-foto varchar(20)
+foto varchar(20),
+moderador int(50),
 
 ) ;
 
-CREATE TABLE `grupos` (
-id int(10) auto_increment,
-nombre Varchar(50),
-foto varchar(20)
-
+CREATE TABLE `usuario_grupos` (
+id int(1000) auto_increment,
+id_grupo int(100),
+email_usuario varchar(100)
+PRIMARY KEY (id),
+FOREIGN KEY (id_grupo) REFERENCES grupos (id) on delete cascade,
+FOREIGN KEY (email_usuario) REFERENCES usuarios (email) on delete cascade
 ) ;
 
 
