31c3ccbb9c9697d08b63e110dac5f806f001e7af
A752779 <A752779@10.0.1.113>
2019-05-13 16:07:46 +0200

    se ralla que flipas

--- nuevoGrupo.php
+++ nuevoGrupo.php
@@ -15,9 +15,10 @@ $consulta = $mysqli->query("select * from users ");
             <input type="text" class="form-control" id="exampleFormControlInput1" >
         </div>
         <div class="form-group">
-            <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+            <br><br>
+            <button class="btn btn-info " data-toggle="modal" data-target="#exampleModal" onclick="agregaModal()">Agregar Usuario </button>
             <div id="cajaAñadirUsuario" class>
-                <a onclick="">agregar usuario +</a>
+                
                 
             </div>
          
@@ -25,9 +26,34 @@ $consulta = $mysqli->query("select * from users ");
         
    
 </form>
+    <!--Modal-->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+    
+    
 
 
 <script>
+    function agregaModal(){
+        
+    }
 
 </script>   
 </html>
