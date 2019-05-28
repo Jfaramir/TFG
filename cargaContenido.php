f7aa99e9dd30097a709033fa117a71532000e2ec
pablo <pablo@192.168.1.43>
2019-05-28 22:53:05 +0200

    div

--- cargaContenido.php
+++ cargaContenido.php
@@ -1,22 +1,22 @@
 <?php
-
 $idTarea = $_POST['idTarea'];
-
-
 ?>
 
 <html>
 
     <!--aqui se van a cargar las notas -->
-    <div id="cajaNotas">  
+    <div id="cajaNotas" style="float: left">  
 
     </div>
     <!--aqui se van a cargar el calendario -->
     <div id="cajaCalendario"> 
-
+        
     </div>
 
-    <div></div>
+    <div id="cajaCosa">
+
+
+    </div>
 
 
 
@@ -27,21 +27,35 @@ $idTarea = $_POST['idTarea'];
 
 
     <script>
-        
+
         var _idTarea = <?php $idTarea ?>
 //funcion   que carga las notas el la caja html
         function cargaNotas() {
-            ('#cajaNotas').load('notas.php',{
-                idTarea: _idTarea
-            });
-        }
-        function cargaCalendario() {
-            ('#cajaCalendario').load('calendario.php',{
+            ('#cajaNotas').load('notas.php', {
                 idTarea: _idTarea
             });
         }
         
-        cargaNotas() ;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    
+    function cargaCalendario() {
+            ('#cajaCalendario').load('calendario.php', {
+                idTarea: _idTarea
+            });
+        }
+
+     
     </script>   
 </html>
 
